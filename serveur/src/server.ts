3a0645d8a7797cc291218df3c3bbd7a3e8a0eae7
Kaboufl <r.laurence@geeklab-factory.com>
2025-01-16 16:56:31 +0100

    ajout de la dépendance CORS et configuration du middleware dans le serveur

--- serveur/src/server.ts
+++ serveur/src/server.ts
@@ -1,12 +1,18 @@
 import express, {Request, Response} from 'express';
 import knex from 'knex';
+import cors from 'cors';
 
 import { db } from './db/db';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+}
+
+app.get('/', cors(), (req: Request, res: Response) => {
     db.table('users').where('id', 1).first().then((user) => {
         res.json({ message: `Hello World! ${user?.name}` });
     });
