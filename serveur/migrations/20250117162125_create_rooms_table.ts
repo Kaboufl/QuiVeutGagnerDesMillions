a24518273db175352684a237038e38b3c8721637
Kaboufl <mw3pumat8@gmail.com>
2025-01-18 00:31:47 +0100

    Remove users table migration and add rooms table migration

---
+++ serveur/migrations/20250117162125_create_rooms_table.ts
@@ -0,0 +1,14 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('rooms', (table) => {
+        table.string('room_id').primary();
+        
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
