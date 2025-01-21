f2d636c954ac86e71c0c2685c5015ef6578d41d6
Kaboufl <mw3pumat8@gmail.com>
2025-01-21 18:18:55 +0100

    Enhance room joining functionality; add username handling and player management on join and disconnect

--- serveur/src/server.ts
+++ serveur/src/server.ts
@@ -40,43 +40,71 @@ app.post('/request-room', (req: Request, res: Response) => {
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
 
-  socket.on('room', ({ roomName, questionnaireId }) => {
-      socket.join(roomName);
-      console.log(`User joined room: ${roomName} with questionnaireId: ${questionnaireId}`);
+    socket.on('room', ({ roomName, questionnaireId }) => {
+        socket.join(roomName);
+        console.log(`User joined room: ${roomName} with questionnaireId: ${questionnaireId}`);
 
-      const clients = io.sockets.adapter.rooms.get(roomName);
-      const clientIds = clients ? Array.from(clients) : [];
-      console.log('clients', clientIds);
+        const clients = io.sockets.adapter.rooms.get(roomName);
+        const clientIds = clients ? Array.from(clients) : [];
+        console.log('clients', clientIds);
 
-      io.to(roomName).emit('roomData', {
-          roomName: roomName,
-          clientIds: clientIds
-      });
-  });
+        io.to(roomName).emit('roomData', {
+            roomName: roomName,
+            clientIds: clientIds
+        });
+    });
 
-  socket.on('disconnect', () => {
-      console.log('user disconnected');
-  });
+    socket.on('join-lobby', async ({ lobbyId, username }) => {
+        console.log(lobbyId)
+        const lobbyRequest = db('rooms').where('room_id', lobbyId);
+        const lobby = await lobbyRequest.first();
 
-  socket.on('join-lobby', async ({ lobbyId }) => {
-    const room = await db('rooms').where('room_id', lobbyId).first();
 
-    console.log(room)
+        if (lobby && username) {
+            socket.join(lobbyId);
+            const playerId = await db('players').returning('id')
+                .insert({
+                    "room_id": lobbyId,
+                    "username": username,
+                    "user_identifier": socket.id
+                })
 
-    if (room) {
-        socket.join(lobbyId)
-        console.log(`User joined with code ${lobbyId}`);
-        socket.emit('roomData', { 
-            message: `Joined room ${lobbyId}`,
-            lobbyId            
-        })
-    } else {
-        socket.emit('no-room', { message: 'no such room' });
-    }
+            if (!lobby.master_id) {
+                await lobbyRequest.update({ master_id: playerId });
+                console.log(`User ${username} with socketID ${socket.id} is now the master of the room ${lobby.id}`);
+            }
 
-  })
+            const playerIds = Array.from(io.sockets.adapter.rooms.get(lobbyId) || []);
+            console.log('players IDs', playerIds);
+            const players = playerIds.map(async (client) => {
+                return await db('players').where('user_identifier', client).first();
+            })
+
+            console.log(`User ${username} joined with code ${lobbyId}`);
+            socket.emit('roomData', { 
+                message: `Joined room ${lobbyId}`,
+                players,
+                lobbyId            
+            })
+        } else {
+            socket.emit('no-room', { message: 'no such room' });
+        }
+    });
+
+    socket.on('disconnect', async () => {
+        const request = db('players').where('user_identifier', socket.id);
+        const player = await request.first();
+        
+        if (player !== undefined) {
+            request.delete().then((res) => {
+                console.log(`${player.username} disconnected from room ${player.room_id}`)
+            })
+        } else {
+            console.log(`User with ID ${socket.id} disconnected, but was not enlisted in a room`)
+        }
+    });
 });
 
 
