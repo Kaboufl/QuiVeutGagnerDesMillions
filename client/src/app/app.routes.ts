1adb2f6fefc469a6a9e37433a0adfe7de037c730
Kaboufl <mw3pumat8@gmail.com>
2025-01-21 18:20:32 +0100

    Add route for game lobby; link LobbyComponent to lobby_id parameter

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LobbyComponent } from './game/lobby/lobby.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: HomeComponent
+    }, {
+        path: "game/:lobby_id",
+        component: LobbyComponent
     }
 ];
